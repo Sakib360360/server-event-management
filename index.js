5449316fb6bf730ae926b07e5b8b560edc357a43
Sakib360360 <saakibabrar@gmail.com>
2023-11-03 12:31:36 +0600

    liked

--- index.js
+++ index.js
@@ -15,106 +15,136 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    const eventsCollection = client.db("EventsDB").collection("events");
-    const usersCollection = client.db("EventsDB").collection("users");
+        const eventsCollection = client.db("EventsDB").collection("events");
+        const usersCollection = client.db("EventsDB").collection("users");
+        const likedCollection = client.db("EventsDB").collection("likedEvents");
 
-    // get all the events based on user's email
-    app.get("/events", async(req, res)=>{
-        // const email = req.query.email;
-        const query = {};
-        const result = await eventsCollection.find(query).toArray();
-        res.send(result);
-    });
+        // get all the events based on user's email
+        app.get("/events", async (req, res) => {
+            // const email = req.query.email;
+            const query = {};
+            const result = await eventsCollection.find(query).toArray();
+            res.send(result);
+        });
 
-    // save the event into the database
-    app.post("/events", async(req, res)=>{
-        const event = req.body;
-        const result = await eventsCollection.insertOne(event);
-        res.send(result);
-    });
+        // save the event into the database
+        app.post("/events", async (req, res) => {
+            const event = req.body;
+            const result = await eventsCollection.insertOne(event);
+            res.send(result);
+        });
 
-    // update a event in database
-    app.patch("/events/:id", async(req, res)=>{
-        const id = req.params.id;
-        const event = req.body;
+        // update a event in database
+        app.patch("/events/:id", async (req, res) => {
+            const id = req.params.id;
+            const event = req.body;
 
-        const query = {_id: new ObjectId(id)};
-        
-        const updateDoc = {
-            $set: {
-                name: event.name,
-                description: event.description,
-                image: event.image,
-                date: event.date,
-                time: event.time,
-                location: event.location,
-                category: event.category,
-                tickets: event.tickets
+            const query = { _id: new ObjectId(id) };
+
+            const updateDoc = {
+                $set: {
+                    name: event.name,
+                    description: event.description,
+                    image: event.image,
+                    date: event.date,
+                    time: event.time,
+                    location: event.location,
+                    category: event.category,
+                    tickets: event.tickets
+                }
             }
-        }
 
-        const result = await eventsCollection.updateOne(query, updateDoc);
-        res.send(result);
-    });
-
-
-    // delete from the database
-    app.delete("/events/:id", async(req, res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await eventsCollection.deleteOne(query);
-        res.send(result);
-    });
-
-    
-      // save user into the database
-      app.post("/users", async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email };
-        const userExists = await usersCollection.findOne(query);
-  
-        if (userExists) {
-          return res.send({ message: "user already exists" });
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-          // get all the users from database
-    app.get("/users", async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
+            const result = await eventsCollection.updateOne(query, updateDoc);
+            res.send(result);
+        });
 
 
-    
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // delete from the database
+        app.delete("/events/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await eventsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // save user into the database
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const userExists = await usersCollection.findOne(query);
+
+            if (userExists) {
+                return res.send({ message: "user already exists" });
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        //   save to favorite
+        app.post('/addToLiked', async (req, res) => {
+            const user = req.body; // Assuming the user object is sent in the request body
+        
+            try {
+              // Check if the user already exists in the favorites collection
+              const existingUser = await likedCollection('favorites').findOne({ username: user.email });
+        
+              if (existingUser) {
+                // User already exists, update the entire document
+                await likedCollection('favorites').updateOne(
+                  { username: user.email },
+                  { $set: { likedEvents: user.likedEvents } }
+                );
+              } else {
+                // User doesn't exist, create a new document
+                await likedCollection('favorites').insertOne({
+                  username: user.email,
+                  likedEvents: user.likedEvents,
+                });
+              }
+        
+              res.status(200).json({ message: 'User added successfully' });
+            } catch (error) {
+              console.error('Error adding user:', error);
+              res.status(500).json({ error: 'Internal Server Error' });
+            }
+          });
+        // get all the users from database
+        app.get("/users", async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        });
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("server running in localhost");
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`visit http://localhost:${port}`);
 })
